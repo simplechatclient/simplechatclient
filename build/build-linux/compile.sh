43bf5b652497e7651e142bca7c374ec21867b308
mero <piotr.luczko@gmail.com>
2016-06-12 23:14:24 +0200

    initial fix for macos

--- build/build-linux/compile.sh
+++ build/build-linux/compile.sh
@@ -2,14 +2,14 @@
 cp /usr/bin/scc ./pkg-debian/usr/bin/scc
 cp -r /usr/share/scc/* ./pkg-debian/usr/share/scc/
 
-chmod 755 ./pkg-debian/usr/bin/scc
-find ./pkg-debian/usr/share/scc/ -type d -exec chmod 755 {} \;
-find ./pkg-debian/usr/share/scc/ -type f -exec chmod 644 {} \;
+chmod 0755 ./pkg-debian/usr/bin/scc
+find ./pkg-debian/usr/share/scc/ -type d -exec chmod 0755 {} \;
+find ./pkg-debian/usr/share/scc/ -type f -exec chmod 0644 {} \;
 
 cd pkg-debian
 find . -type f ! -regex '.*\.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > DEBIAN/md5sums 
 cd ..
 
-chmod 644 ./pkg-debian/DEBIAN/md5sums
+chmod 0644 ./pkg-debian/DEBIAN/md5sums
 fakeroot dpkg -b pkg-debian/ scc-2.2.0.0.deb
 fakeroot alien -c -k -r scc-2.2.0.0.deb
